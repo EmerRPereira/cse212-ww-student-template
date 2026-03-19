b43edd06b69e253c89484d87c38da862fef44e4d
EmerRPereira <pemerson@escola.pr.gov.br>
2026-03-19 10:35:32 -0300

    Corereção para execução

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,4 +1,5 @@
-﻿public static class DisplaySums {
+﻿/*
+public static class DisplaySums {
     public static void Run() {
         DisplaySumPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
         // Should show something like (order does not matter):
@@ -43,4 +44,56 @@
         valoresVistos.Add(n);
     }
 
+}
+
+*/
+
+using System;
+using System.Collections.Generic;
+
+public static class DisplaySums 
+{
+    public static void Run() 
+    {
+        // Usando sintaxe compatível com versões mais antigas do C#
+        DisplaySumPairs(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        // Deve mostrar algo como:
+        // 6 4
+        // 7 3
+        // 8 2
+        // 9 1 
+
+        Console.WriteLine("------------");
+        DisplaySumPairs(new int[] { -20, -15, -10, -5, 0, 5, 10, 15, 20 });
+        // Deve mostrar:
+        // 10 0
+        // 15 -5
+        // 20 -10
+
+        Console.WriteLine("------------");
+        DisplaySumPairs(new int[] { 5, 11, 2, -4, 6, 8, -1 });
+        // Deve mostrar:
+        // 8 2
+        // -1 11
+    }
+
+    private static void DisplaySumPairs(int[] numbers) 
+    {
+        // HashSet para armazenar números já vistos (busca O(1))
+        var valoresVistos = new HashSet<int>();
+        
+        foreach (var n in numbers)
+        {
+            int complemento = 10 - n;  // Qual número precisamos para somar 10?
+            
+            // Se o complemento já foi visto, encontramos um par
+            if (valoresVistos.Contains(complemento))
+            {
+                Console.WriteLine($"{n} {complemento}");
+            }
+            
+            // Adiciona o número atual ao conjunto para verificações futuras
+            valoresVistos.Add(n);
+        }
+    }
 }
